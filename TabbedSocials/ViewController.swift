5651ce93f83eb70adf883c93acad65aaca59d1b2
Mariah Stoakley <mariahstoakley@gmail.com>
2022-03-22 15:54:05 -0500

    Initial Commit

---
+++ TabbedSocials/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabbedSocials
+//
+//  Created by Mariah Stoakley on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
